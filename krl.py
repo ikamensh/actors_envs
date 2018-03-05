25bf1094996ced674b184d4e89bd8d9e9148954c
Ilya <ikamenshchikov@gmail.com>
2018-03-05 09:36:49 +0100

    Tensorboard integration

--- krl.py
+++ krl.py
@@ -18,32 +18,37 @@ from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 
-INPUT_SHAPE = (84, 84)
+INPUT_SHAPE = (4,)
 WINDOW_LENGTH = 4
 
 
 class AtariProcessor(Processor):
-    def process_observation(self, observation):
-        assert observation.ndim == 3  # (height, width, channel)
-        img = Image.fromarray(observation)
-        img = img.resize(INPUT_SHAPE).convert('L')  # resize and convert to grayscale
-        processed_observation = np.array(img)
-        assert processed_observation.shape == INPUT_SHAPE
-        return processed_observation.astype('uint8')  # saves storage in experience memory
 
-    def process_state_batch(self, batch):
-        # We could perform this processing step in `process_observation`. In this case, however,
-        # we would need to store a `float32` array instead, which is 4x more memory intensive than
-        # an `uint8` array. This matters if we store 1M observations.
-        processed_batch = batch.astype('float32') / 255.
-        return processed_batch
+    def __init__(self):
+        self.n_steps = np.zeros([1,], dtype=np.float32)
+        self.total_reward = 0.
+
+    def process_step(self, observation, reward, done, info):
+
+        reward /= 200
+        self.total_reward += reward
+
+        if done:
+            if self.total_reward < 1:
+                reward = self.total_reward - 1
+            self.total_reward = 0.
+            self.n_steps *= 0.
+        
+        super().process_step(observation, reward, done, info)
+        return observation, reward, done, info
 
     def process_reward(self, reward):
         return np.clip(reward, -1., 1.)
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode', choices=['train', 'test'], default='train')
-parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v4')
+parser.add_argument('--env-name', type=str, default='CartPole-v0')
 parser.add_argument('--weights', type=str, default=None)
 args = parser.parse_args()
 
@@ -57,18 +62,12 @@ nb_actions = env.action_space.n
 input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 
 def build_model():
+    input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
     model = Sequential()
-        # (width, height, channels)
-    model.add(Permute((2, 3, 1), input_shape=input_shape))
-    model.add(Convolution2D(32, 8, 8, subsample=(4, 4)))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(64, 3, 3, subsample=(1, 1)))
-    model.add(Activation('relu'))
-    model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('relu'))
+    model.add(Flatten(input_shape=input_shape))
+    model.add(Dense(512, activation='selu'))
+    model.add(Dense(512, activation='selu'))
+    model.add(Dense(512, activation='selu'))
     model.add(Dense(nb_actions))
     model.add(Activation('linear'))
     print(model.summary())
